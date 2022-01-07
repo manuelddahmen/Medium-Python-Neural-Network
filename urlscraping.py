687f212630053875c69bcb08903a72bece6c7140
dathewolf <manuel.dahmen@gmx.com>
2022-01-07 19:33:10 +0100

    Initial commit
    Signed-off-by: dathewolf <manuel.dahmen@gmx.com>

--- urlscraping.py
+++ urlscraping.py
@@ -60,37 +60,36 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, sleep_between_interact
         thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
         number_results = len(thumbnail_results)
 
-        print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
+        if number_results > 0:
+            print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
+            for img in thumbnail_results[results_start:number_results]:
+                # try to click every thumbnail such that we can get the real image behind it
+                try:
+                    img.click()
+                    time.sleep(sleep_between_interactions)
+                except Exception:
+                    continue
+                # extract image urls
+                actual_images = wd.find_elements_by_css_selector('img.n3VNCb')
+                for actual_image in actual_images:
+                    if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
+                        image_urls.add(actual_image.get_attribute('src'))
 
-        for img in thumbnail_results[results_start:number_results]:
-            # try to click every thumbnail such that we can get the real image behind it
-            try:
-                img.click()
-                time.sleep(sleep_between_interactions)
-            except Exception:
-                continue
+                image_count = len(image_urls)
 
-            # extract image urls
-            actual_images = wd.find_elements_by_css_selector('img.n3VNCb')
-            for actual_image in actual_images:
-                if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
-                    image_urls.add(actual_image.get_attribute('src'))
+                if len(image_urls) >= max_links_to_fetch:
+                    print(f"Found: {len(image_urls)} image links, done!")
+                else:
+                    print("Found:", len(image_urls), "image links, looking for more ...")
+                    time.sleep(1)
+                    # load_more_button = wd.find_element_by_css_selector(".mye4qd")
+                    # if load_more_button:
+                    #    wd.execute_script("document.querySelector('.mye4qd').click();")
 
-            image_count = len(image_urls)
-
-            if len(image_urls) >= max_links_to_fetch:
-                print(f"Found: {len(image_urls)} image links, done!")
-                break
+                # move the result startpoint further down
+            results_start = len(thumbnail_results)
         else:
-            print("Found:", len(image_urls), "image links, looking for more ...")
-            time.sleep(0.30)
-            return
-            load_more_button = wd.find_element_by_css_selector(".mye4qd")
-            if load_more_button:
-                wd.execute_script("document.querySelector('.mye4qd').click();")
-
-        # move the result startpoint further down
-        results_start = len(thumbnail_results)
+            return image_count, image_urls
 
     return image_count, image_urls
 
